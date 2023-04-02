b4ed90ed1fa0b16e46ef3c7a5ea242095e0be621
Robert Marton <matula80@gmail.com>
2023-04-02 10:58:04 +0200

    Creat components

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CompetencesComponent } from './competences/competences.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CompetencesComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
