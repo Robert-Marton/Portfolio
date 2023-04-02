39d23f3bd36db79eb3486f6c4cfe76c009e0a082
Robert Marton <matula80@gmail.com>
2023-04-02 11:25:27 +0200

    Install bootstrap module

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CompetencesComponent } from './competences/competences.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AboutMeComponent } from './about-me/about-me.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
