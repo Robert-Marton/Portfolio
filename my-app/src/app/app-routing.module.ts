df59670c1702b2305a9a574c1c151120da242555
Robert Marton <matula80@gmail.com>
2023-04-02 11:16:44 +0200

    Routing

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CompetencesComponent } from './competences/competences.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'competences', component:CompetencesComponent},
+  {path:'about-me', component:AboutMeComponent},
+  {path:'', redirectTo: '/home', pathMatch: 'full'},
+  {path:'**', component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
